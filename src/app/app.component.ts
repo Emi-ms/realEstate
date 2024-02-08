9be67b0040b1eefdde77cb38efc0d1103857c0c0
Emi-ms <cabeemil86@gmail.com>
2024-02-08 19:10:00 +0100

    New Angular Project

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+
+    <router-outlet />
+    </main>
+  `,
+  styles: [],
+})
+export class AppComponent {
+  title = 'realEstate';
+}

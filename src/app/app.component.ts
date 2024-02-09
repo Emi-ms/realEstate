5faa17677d5d19bc12981f5cde8a6582ed3e17f7
Emi-ms <cabeemil86@gmail.com>
2024-02-09 09:45:56 +0100

    Fix imports and add search functionality to home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [
+    RouterOutlet,
+    HomeComponent,],
   template: `
     <main>
       <header class="brand-name">
